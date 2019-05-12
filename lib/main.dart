6e4755f59ce11aae60e3af20d51fae19c33550a0
emoalb <e_alb@abv.bg>
2019-05-12 21:34:15 +0300

    Some weird stuff is going on

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -30,3 +38,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
