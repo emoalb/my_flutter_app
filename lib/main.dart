2e06d3894177b1bad29ef0216e23a19448ff6208
emoalb <e_alb@abv.bg>
2019-05-12 21:27:40 +0300

    Following flutter academind youtube course

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
+    return new MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpeg'),
-              Text('Food Paradice')
-            ],
+        appBar: AppBar(title: Text('EasyList')),
+        body: Column(children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: RaisedButton(
+              onPressed: () => {},
+              child: Text('Add Product'),
+            ),
           ),
-        ),
+          Card(
+            child: Column(
+              children: <Widget>[
+                Image.asset('assets/food.jpeg'),
+                Text('Food Paradice!')
+              ],
+            ),
+          ),
+        ]),
       ),
     );
   }
